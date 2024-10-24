047c8820da44916ab3780455d10189e842d4a2e2
git961 <120348157+git961@users.noreply.github.com>
2024-10-24 09:52:47 +0900

    敵削除

--- KatiKatiClick/ObjectFile/EnemyFile/SquishEnemy.h
+++ KatiKatiClick/ObjectFile/EnemyFile/SquishEnemy.h
@@ -7,6 +7,7 @@ private:
 public:
     SquishEnemy();
     ~SquishEnemy();
+    void initialize() override;
     void Update()override;
     void Draw()const override;
     void HitReaction(ObjectBase* character) override;
