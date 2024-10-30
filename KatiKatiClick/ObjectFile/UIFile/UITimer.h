639b93b80d5b2755d133018a8f352e3e198e721b
chihiro-hiro <120348424+chihiro-hiro@users.noreply.github.com>
2024-10-30 11:03:04 +0900

    Timerファイル作成

---
+++ KatiKatiClick/ObjectFile/UIFile/UITimer.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "UIBase.h"
+
+class UITimer : public UIBase
+{
+private:
+	int fps_count;
+	int seconds;		// ゲームが終わるまでの秒数
+
+public:
+    UITimer();
+    ~UITimer();
+
+    void Update() override;
+    void Draw() const override;
+};
