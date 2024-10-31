71d7904ff46a9a1acace2ad75459ca07b040e882
git961 <120348157+git961@users.noreply.github.com>
2024-10-31 12:34:22 +0900

    削除

--- KatiKatiClick/ObjectFile/GoalFile/Goal.cpp
+++ KatiKatiClick/ObjectFile/GoalFile/Goal.cpp
@@ -29,7 +29,6 @@ void Goal::Draw() const
 {
 	DrawBox((int)location.x - (int)width / 2, (int)location.y - (int)height / 2, (int)location.x + (int)width / 2, (int)location.y + (int)height / 2, 0xffff00, TRUE);
 	DrawFormatString((int)location.x, (int)location.y, 0xe000000, "goal_cnt:%d", goal_cnt);
-	DrawFormatString((int)location.x, (int)location.y+20, 0xe000000, "posx:%f", location.x);
 
 	//ゴール仮幅
 	//DrawBox(0, SCREEN_HEIGHT - GET_LANE_HEIGHT(2), SCREEN_WIDTH, SCREEN_HEIGHT - GET_LANE_HEIGHT(2) + 5, 0xffff00, TRUE);
