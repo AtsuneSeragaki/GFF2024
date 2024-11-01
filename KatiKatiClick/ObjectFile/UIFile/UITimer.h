ec81238107607960a7441acdc1aa1b3ea321de25
chihiro-hiro <120348424+chihiro-hiro@users.noreply.github.com>
2024-11-01 11:01:21 +0900

    Timerを画像表示に変更

--- KatiKatiClick/ObjectFile/UIFile/UITimer.h
+++ KatiKatiClick/ObjectFile/UIFile/UITimer.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "UIBase.h"
+#include <vector>
 
 class UITimer : public UIBase
 {
 private:
 	int fps_count;
-	int seconds;		// ゲームが終わるまでの秒数
+	int seconds;		            // ゲームが終わるまでの秒数
+    std::vector<int> images_data;   // 数字画像
+    int image_num[2];               // 描画する画像番号
 
 public:
     UITimer();
