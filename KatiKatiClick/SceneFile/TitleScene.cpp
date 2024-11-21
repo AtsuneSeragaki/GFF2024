8f0a2f60f0044375e80e81b6c56bb04dd362c18e
AtsuneSeragaki <120348226+AtsuneSeragaki@users.noreply.github.com>
2024-11-21 14:59:36 +0900

    タイトル作成中

--- KatiKatiClick/SceneFile/TitleScene.cpp
+++ KatiKatiClick/SceneFile/TitleScene.cpp
@@ -1,14 +1,20 @@
 #include "TitleScene.h"
-
-#include "../UtilityFile/MouseInput.h"
+#include "../ObjectFile/PlayerFile/Cursor.h"
+#include "GameMainScene.h"
+#include "../UtilityFile//Geometry.h"
 #include "DxLib.h"
+#include <math.h>
 
 TitleScene::TitleScene()
 {
-	x1 = 75;
-	x2 = 255;
-	y1 = 300;
-	y2 = 300;
+	x1 = 75.0f;
+	x2 = 255.0f;
+	y1 = 300.0f;
+	y2 = 300.0f;
+
+	mouseinput = new MouseInput;
+
+	select = -1;
 }
 
 TitleScene::~TitleScene()
@@ -17,17 +23,137 @@ TitleScene::~TitleScene()
 
 void TitleScene::Update()
 {
+	if (mouseinput->GetMouseState() == eMouseInputState::eClick)
+	{
+		if (HitBoxCircle(x1, y1,mouseinput->GetMouseLocation(),12.0f) == true)
+		{
+			select = 0;
+		}
+		else
+		{
+			if (HitBoxCircle(x2, y2, mouseinput->GetMouseLocation(), 12.0f) == true)
+			{
+				select = 1;
+			}
+		}
+	}
+	
 }
 
 void TitleScene::Draw() const
 {
-	DrawBox(x1 - BOX_WIDTH / 2, y1 - BOX_HEIGHT / 2, x1 + BOX_WIDTH / 2, y1 + BOX_HEIGHT / 2, 0xffffff, TRUE);
+	DrawBoxAA(x1 - (float)BOX_WIDTH / 2, y1 - (float)BOX_HEIGHT / 2, x1 + (float)BOX_WIDTH / 2, y1 + (float)BOX_HEIGHT / 2, 0xffffff, TRUE);
 	DrawString(x1, y1, "START", 0x000000);
-	DrawBox(x2 - BOX_WIDTH / 2, y2 - BOX_HEIGHT / 2, x2 + BOX_WIDTH / 2, y2 + BOX_HEIGHT / 2, 0xffffff, TRUE);
+	DrawBoxAA(x2 - (float)BOX_WIDTH / 2, y2 - (float)BOX_HEIGHT / 2, x2 + (float)BOX_WIDTH / 2, y2 + (float)BOX_HEIGHT / 2, 0xffffff, TRUE);
 	DrawString(x2, y2, "END", 0x000000);
 }
 
 AbstractScene* TitleScene::Change()
 {
+	switch (select)
+	{
+	case 0:
+		return new GameMainScene();
+		break;
+
+	case 1:
+		//return new EndScene();
+		break;
+
+	default:
+		break;
+	}
+
 	return this;
+}
+
+float TitleScene::DistanceSqrf(float box_x, float box_y, float circle_x, float circle_y)
+{
+	float dx = circle_x - box_x;
+	float dy = circle_y - box_y;
+
+	return (dx * dx) + (dy * dy);
+}
+
+bool TitleScene::HitBoxCircle(float box_x, float box_y, Vector2D location, float radius)
+{
+	float box_left, box_right, box_top, box_bottom;
+	float circle_x, circle_y;
+	float rad;//半径
+	bool hit_result = false;//当たったか
+
+	//自分が四角だったら
+	box_left = box_x - BOX_WIDTH / 2;
+	box_top =  box_y - BOX_HEIGHT / 2;
+	box_right = box_x + BOX_WIDTH / 2;
+	box_bottom = box_y + BOX_HEIGHT / 2;
+
+	circle_x = location.x;
+	circle_y = location.y;
+	rad = radius;
+	
+	//circleの端がボックスの上下左右に入っているか
+	if ((circle_x > box_left - rad) &&
+		(circle_x < box_right + rad) &&
+		(circle_y > box_top - rad) &&
+		(circle_y < box_bottom + rad))
+	{
+		hit_result = true;
+		float r = rad * rad;//半径＊半径
+
+		//circleのｘが左より小さかったら
+		if (circle_x < box_left)
+		{
+			//左上の座標より上にいたら
+			if (circle_y < box_top)
+			{
+				//左上の頂点との判定
+				if ((DistanceSqrf(box_left, box_top, circle_x, circle_y)) >= r)
+				{
+					//二乗より大きいので当たってない
+					hit_result = false;
+				}
+			}
+			else
+			{
+				//circleのｘが左下の座標より下にいたら
+				if (circle_y > box_bottom)
+				{
+					//左下の頂点との判定
+					if (DistanceSqrf(box_left, box_bottom, circle_x, circle_y) >= r)
+					{
+						hit_result = false;
+					}
+				}
+			}
+		}
+		else
+		{
+			//右
+			if (circle_x > box_right)
+			{
+				//右上
+				if (circle_y < box_top)
+				{
+					if ((DistanceSqrf(box_right, box_top, circle_x, circle_y)) >= r)
+					{
+						hit_result = false;
+					}
+				}
+				else
+				{
+					//右下
+					if (circle_y > box_bottom)
+					{
+						if (DistanceSqrf(box_right, box_bottom, circle_x, circle_y) >= r)
+						{
+							hit_result = false;
+						}
+					}
+				}
+
+			}
+		}
+	}
+	return hit_result;
 }
--- KatiKatiClick/SceneFile/TitleScene.h
+++ KatiKatiClick/SceneFile/TitleScene.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "AbstractScene.h"
+#include "../UtilityFile/MouseInput.h"
 
 #define BOX_WIDTH  100
 #define BOX_HEIGHT 100
@@ -8,7 +9,9 @@
 class TitleScene : public AbstractScene
 {
 private:
-	int x1, x2, y1, y2; // ボタンの座標
+	MouseInput* mouseinput;
+	float x1, x2, y1, y2; // ボタンの座標
+	int select; // プレイヤーが選んだメニュー
 
 public:
 	TitleScene();
@@ -17,5 +20,9 @@ public:
 	void Update() override;
 	void Draw() const override;
 	AbstractScene* Change() override;
+
+private:
+	float DistanceSqrf(float box_x, float box_y, float circle_x, float circle_y);
+	bool HitBoxCircle(float box_x,float box_y,Vector2D location,float radius);//四角と円の当たり判定
 };
 
