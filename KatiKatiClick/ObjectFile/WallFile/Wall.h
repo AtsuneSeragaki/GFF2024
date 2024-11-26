7cf125e8f360f60637e8530494d1a19b4b3e9da6
git961 <120348157+git961@users.noreply.github.com>
2024-11-26 12:11:46 +0900

    壁点滅

--- KatiKatiClick/ObjectFile/WallFile/Wall.h
+++ KatiKatiClick/ObjectFile/WallFile/Wall.h
@@ -14,12 +14,15 @@ private:
     int img_num;//画像切替表示用
     int smoke_cnt;
     int change_cnt;//画像切り替えカウント
+    int flash_cnt;
+    int off_flash_cnt;//flashとめるまでのカウント
 
     Vector2D damage_pos;
     bool damage_display;
     bool can_shake;
-
     bool smoke_flg;
+    bool can_damage_flash;//点滅するか
+    bool flash_flg;
 
     int se; // 効果音
     int se_2; // 効果音
