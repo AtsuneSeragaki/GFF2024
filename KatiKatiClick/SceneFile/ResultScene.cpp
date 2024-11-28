34608702b5fd7a68f28860e6285df5532d46249a
AtsuneSeragaki <120348226+AtsuneSeragaki@users.noreply.github.com>
2024-11-28 15:00:20 +0900

    リザルト星処理

--- KatiKatiClick/SceneFile/ResultScene.cpp
+++ KatiKatiClick/SceneFile/ResultScene.cpp
@@ -3,16 +3,17 @@
 #include "GameMainScene.h"
 #include "../UtilityFile/ResourceManager.h"
 #include "../UtilityFile/Define.h"
+#include "../UtilityFile/Define.h"
 #include "DxLib.h"
 #include <math.h>
 
 
 ResultScene::ResultScene(bool is_game_clear, int goal_num)
 {
-	x1 = 85.0f;
-	x2 = 275.0f;
-	y1 = 600.0f;
-	y2 = 600.0f;
+	x1 = 85.0f + 20.0f;
+	x2 = 275.0f - 20.0f;
+	y1 = 570.0f;
+	y2 = 570.0f;
 
 	cursor = new Cursor;
 
@@ -34,6 +35,8 @@ ResultScene::ResultScene(bool is_game_clear, int goal_num)
 	star_images[1] = tmp[0];
 	tmp = rm->GetImages("Resource/Images/Result/fire.png");
 	fire_image = tmp[0];
+	tmp = rm->GetImages("Resource/Images/Opening/pizza_margherita.png");
+	pizza_image = tmp[0];
 
 	// 音データ読み込み
 	int tmp_bgm;
@@ -45,7 +48,40 @@ ResultScene::ResultScene(bool is_game_clear, int goal_num)
 	// 音量変更
 	ChangeVolumeSoundMem(190, bgm);
 
-	extrate = 0.2f;
+	pizza_angle = 0.0f;
+
+	for (int i = 0; i < 3; i++)
+	{
+		star_hp[i] = -1;
+		star_x[i] = STAR_X + 85.0f * (float)i;
+		star_gold[i] = false;
+	}
+
+	star_y[0] = STAR_Y;
+	star_y[1] = STAR_Y - 40.0f;
+	star_y[2] = STAR_Y;
+
+	star_angle[0] = -PI / 180.0f * 15.0f;
+	star_angle[1] = 0.0f;
+	star_angle[2] = PI / 180.0f * 15.0f;
+
+	star_extrate[0] = 0.15f;
+	star_extrate[1] = 0.2f;
+	star_extrate[2] = 0.15f;
+
+	star_gold_extrate[0] = 0.15f;
+	star_gold_extrate[1] = 0.2f;
+	star_gold_extrate[2] = 0.15f;
+
+	/*star_gold_extrate[0] = 0.55f;
+	star_gold_extrate[1] = 0.6f;
+	star_gold_extrate[2] = 0.55f;*/
+
+	for (int i = 0; i < goal_num; i++)
+	{
+		star_gold[i] = true;
+		star_hp[i] = 10;
+	}
 }
 
 ResultScene::~ResultScene()
@@ -63,85 +99,83 @@ void ResultScene::Update()
 
 	cursor->Update();
 
-	if (cursor->GetPState() == P_State::attack && cursor->GetCanHit()==true)
-	{
-		if (HitBoxCircle(x1, y1, cursor->GetLocation(), cursor->GetRadius()) == true)
-		{
-			select = 0;
-		}
-		else
-		{
-			if (HitBoxCircle(x2, y2, cursor->GetLocation(), cursor->GetRadius()) == true)
-			{
-				select = 1;
-			}
-		}
-	}
-	else
-	{
-		if (HitBoxCircle(x1, y1, cursor->GetLocation(), cursor->GetRadius()) == true)
-		{
-			on_button = 0;
-		}
-		else
-		{
-			if (HitBoxCircle(x2, y2, cursor->GetLocation(), cursor->GetRadius()) == true)
-			{
-				on_button = 1;
-			}
-			else
-			{
-				on_button = -1;
-			}
-		}
-	}
+	HitCheck();
+
+	StarMove();
+
+	ChangePizzaAngle();
+
 }
 
 void ResultScene::Draw() const
 {
-	DrawString(160, 100, "RESULT", 0xffffff);
+	DrawBox(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0x333333,TRUE);
+	DrawString(160, 75, "RESULT", 0xffffff);
 
 	// ゲームクリア、ゲームオーバーの表示
 	if (is_clear == true)
 	{
-		DrawString(140, 160, "GAME CLEAR", 0xffffff);
+		DrawString(140, 110, "GAME CLEAR", 0xffffff);
 	}
 	else
 	{
-		DrawString(145, 160, "GAME OVER", 0xffffff);
+		DrawString(145, 120, "GAME OVER", 0xffffff);
 	}
 
 	// 星の表示
 	switch (star_num)
 	{
 	case 1:
-		DrawRotaGraph2F(STAR_X, STAR_Y, 20.0f, 20.0f, 0.2f, 0,star_images[0],TRUE);
-		DrawRotaGraph2F(STAR_X + 90.0f, STAR_Y - 20.0f, 20.0f, 20.0f, 0.2f, 0, star_images[0], TRUE);
-		DrawRotaGraph2F(STAR_X + 180.0f, STAR_Y, 20.0f, 20.0f, 0.2f,0, star_images[0], TRUE);
-		DrawRotaGraph2F(STAR_X, STAR_Y, 20.0f, 20.0f, extrate, 0, star_images[1], TRUE);
+		// 銀の星描画
+		for (int i = 0; i < 3; i++)
+		{
+			DrawRotaGraph2F(star_x[i], star_y[i], 250.0f, 250.0f, star_extrate[i], star_angle[i], star_images[0], TRUE);
+		}
+
+		// 金の星描画
+		for (int i = 0; i < star_num; i++)
+		{
+			DrawRotaGraph2F(star_x[i], star_y[i], 250.0f, 250.0f, star_gold_extrate[i], star_angle[i], star_images[1], TRUE);
+		}
 		break;
 
 	case 2:
-		DrawRotaGraph2F(STAR_X, STAR_Y, 20.0f, 20.0f, 0.2f, 0, star_images[0], TRUE);
-		DrawRotaGraph2F(STAR_X + 90.0f, STAR_Y - 20.0f, 20.0f, 20.0f, 0.2f, 0, star_images[0], TRUE);
-		DrawRotaGraph2F(STAR_X + 180.0f, STAR_Y, 20.0f, 20.0f, 0.2f, 0, star_images[0], TRUE);
-		DrawRotaGraph2F(STAR_X, STAR_Y, 20.0f, 20.0f, extrate, 0, star_images[1], TRUE);
-		DrawRotaGraph2F(STAR_X + 90.0f, STAR_Y - 20.0f, 20.0f, 20.0f, 0.2f, 0, star_images[1], TRUE);
+		// 銀の星描画
+		for (int i = 0; i < 3; i++)
+		{
+			DrawRotaGraph2F(star_x[i], star_y[i], 250.0f, 250.0f, star_extrate[i], star_angle[i], star_images[0], TRUE);
+		}
+
+		// 金の星描画
+		for (int i = 0; i < star_num; i++)
+		{
+			DrawRotaGraph2F(star_x[i], star_y[i], 250.0f, 250.0f, star_gold_extrate[i], star_angle[i], star_images[1], TRUE);
+		}
 		break;
 
 	case 3:
-		DrawRotaGraph2F(STAR_X, STAR_Y, 20.0f, 20.0f, 0.2f, 0, star_images[0], TRUE);
-		DrawRotaGraph2F(STAR_X + 90.0f, STAR_Y - 20.0f, 20.0f, 20.0f, 0.2f, 0, star_images[0], TRUE);
-		DrawRotaGraph2F(STAR_X + 180.0f, STAR_Y, 20.0f, 20.0f, 0.2f, 0, star_images[0], TRUE);
-		DrawRotaGraph2F(STAR_X, STAR_Y, 20.0f, 20.0f, extrate, 0, star_images[1], TRUE);
-		DrawRotaGraph2F(STAR_X + 90.0f, STAR_Y - 20.0f, 20.0f, 20.0f, 0.2f, 0, star_images[1], TRUE);
-		DrawRotaGraph2F(STAR_X + 180.0f, STAR_Y, 20.0f, 20.0f, 0.2f, 0, star_images[1], TRUE);
+
+		// 銀の星描画
+		for (int i = 0; i < 3; i++)
+		{
+			DrawRotaGraph2F(star_x[i], star_y[i], 250.0f, 250.0f, star_extrate[i], star_angle[i], star_images[0], TRUE);
+		}
+
+		DrawBox(star_x[0] - STAR_WIDTH / 2,star_y[0] - STAR_HEIGHT / 2, star_x[0] + STAR_WIDTH / 2, star_y[0] + STAR_HEIGHT / 2, 0xffffff, TRUE);
+
+		// 金の星描画
+		for (int i = 0; i < star_num; i++)
+		{
+			DrawRotaGraph2F(star_x[i], star_y[i], 250.0f, 250.0f, star_gold_extrate[i], star_angle[i], star_images[1], TRUE);
+		}
 		break;
 
 	default:
-		DrawRotaGraph2F(STAR_X, STAR_Y, 20.0f, 20.0f, 0.2f, 0, star_images[0], TRUE);
-		DrawRotaGraph2F(STAR_X + 90.0f, STAR_Y - 20.0f, 20.0f, 20.0f, 0.2f, 0, star_images[0], TRUE);
-		DrawRotaGraph2F(STAR_X + 180.0f, STAR_Y, 20.0f, 20.0f, 0.2f, 0, star_images[0], TRUE);
+		// 銀の星描画
+		for (int i = 0; i < 3; i++)
+		{
+			DrawRotaGraph2F(star_x[i], star_y[i], 250.0f, 250.0f, star_extrate[i], star_angle[i], star_images[0], TRUE);
+		}
 		break;
 	}
 
@@ -149,26 +183,29 @@ void ResultScene::Draw() const
 	switch (on_button)
 	{
 	case 0:
-		DrawBoxAA(x1 - (float)BOX_WIDTH / 2, y1 - (float)BOX_HEIGHT / 2, x1 + (float)BOX_WIDTH / 2, y1 + (float)BOX_HEIGHT / 2, 0x999999, TRUE);
-		DrawString((x1 - (float)BOX_WIDTH / 2) + 30, (y1 - (float)BOX_HEIGHT / 2) + 20, "RETRY", 0x000000);
-		DrawBoxAA(x2 - (float)BOX_WIDTH / 2, y2 - (float)BOX_HEIGHT / 2, x2 + (float)BOX_WIDTH / 2, y2 + (float)BOX_HEIGHT / 2, 0xffffff, TRUE);
-		DrawString((x2 - (float)BOX_WIDTH / 2) + 30, (y2 - (float)BOX_HEIGHT / 2) + 20, "TITLE", 0x000000);
+		DrawBoxAA(x1 - BOX_WIDTH / 2, y1 - BOX_HEIGHT / 2, x1 + BOX_WIDTH / 2, y1 + BOX_HEIGHT / 2, 0x999999, TRUE);
+		DrawString((x1 - BOX_WIDTH / 2) + 30, (y1 - BOX_HEIGHT / 2) + 20, "RETRY", 0x000000);
+		DrawBoxAA(x2 - BOX_WIDTH / 2, y2 - BOX_HEIGHT / 2, x2 + BOX_WIDTH / 2, y2 + BOX_HEIGHT / 2, 0xffffff, TRUE);
+		DrawString((x2 - BOX_WIDTH / 2) + 30, (y2 - BOX_HEIGHT / 2) + 20, "TITLE", 0x000000);
 		break;
 
 	case 1:
-		DrawBoxAA(x1 - (float)BOX_WIDTH / 2, y1 - (float)BOX_HEIGHT / 2, x1 + (float)BOX_WIDTH / 2, y1 + (float)BOX_HEIGHT / 2, 0xffffff, TRUE);
-		DrawString((x1 - (float)BOX_WIDTH / 2) + 30, (y1 - (float)BOX_HEIGHT / 2) + 20, "RETRY", 0x000000);
-		DrawBoxAA(x2 - (float)BOX_WIDTH / 2, y2 - (float)BOX_HEIGHT / 2, x2 + (float)BOX_WIDTH / 2, y2 + (float)BOX_HEIGHT / 2, 0x999999, TRUE);
-		DrawString((x2 - (float)BOX_WIDTH / 2) + 30, (y2 - (float)BOX_HEIGHT / 2) + 20, "TITLE", 0x000000);
+		DrawBoxAA(x1 - BOX_WIDTH / 2, y1 - BOX_HEIGHT / 2, x1 + BOX_WIDTH / 2, y1 + BOX_HEIGHT / 2, 0xffffff, TRUE);
+		DrawString((x1 - BOX_WIDTH / 2) + 30, (y1 - BOX_HEIGHT / 2) + 20, "RETRY", 0x000000);
+		DrawBoxAA(x2 - BOX_WIDTH / 2, y2 - BOX_HEIGHT / 2, x2 + BOX_WIDTH / 2, y2 + BOX_HEIGHT / 2, 0x999999, TRUE);
+		DrawString((x2 - BOX_WIDTH / 2) + 30, (y2 - BOX_HEIGHT / 2) + 20, "TITLE", 0x000000);
 		break;
 
 	default:
-		DrawBoxAA(x1 - (float)BOX_WIDTH / 2, y1 - (float)BOX_HEIGHT / 2, x1 + (float)BOX_WIDTH / 2, y1 + (float)BOX_HEIGHT / 2, 0xffffff, TRUE);
-		DrawString((x1 - (float)BOX_WIDTH / 2) + 30, (y1 - (float)BOX_HEIGHT / 2) + 20, "RETRY", 0x000000);
-		DrawBoxAA(x2 - (float)BOX_WIDTH / 2, y2 - (float)BOX_HEIGHT / 2, x2 + (float)BOX_WIDTH / 2, y2 + (float)BOX_HEIGHT / 2, 0xffffff, TRUE);
-		DrawString((x2 - (float)BOX_WIDTH / 2) + 30, (y2 - (float)BOX_HEIGHT / 2) + 20, "TITLE", 0x000000);
+		DrawBoxAA(x1 - BOX_WIDTH / 2, y1 - BOX_HEIGHT / 2, x1 + BOX_WIDTH / 2, y1 + BOX_HEIGHT / 2, 0xffffff, TRUE);
+		DrawString((x1 - BOX_WIDTH / 2) + 30, (y1 - BOX_HEIGHT / 2) + 20, "RETRY", 0x000000);
+		DrawBoxAA(x2 - BOX_WIDTH / 2, y2 - BOX_HEIGHT / 2, x2 + BOX_WIDTH / 2, y2 + BOX_HEIGHT / 2, 0xffffff, TRUE);
+		DrawString((x2 - BOX_WIDTH / 2) + 30, (y2 - BOX_HEIGHT / 2) + 20, "TITLE", 0x000000);
 		break;
 	}
+
+	DrawBox(40, 300, 320, 500, 0xffffff, TRUE);
+	DrawRotaGraph2(183, 690, 250, 250, 0.2f, pizza_angle, pizza_image, TRUE);
 
 	cursor->Draw();
 }
@@ -204,7 +241,7 @@ float ResultScene::DistanceSqrf(float box_x, float box_y, float circle_x, float 
 	return (dx * dx) + (dy * dy);
 }
 
-bool ResultScene::HitBoxCircle(float box_x, float box_y, Vector2D location, float radius)
+bool ResultScene::HitBoxCircle(float box_x, float box_y,float width,float height, Vector2D location, float radius)
 {
 	float box_left, box_right, box_top, box_bottom;
 	float circle_x, circle_y;
@@ -212,10 +249,10 @@ bool ResultScene::HitBoxCircle(float box_x, float box_y, Vector2D location, floa
 	bool hit_result = false;//当たったか
 
 	//自分が四角だったら
-	box_left = box_x - BOX_WIDTH / 2;
-	box_top = box_y - BOX_HEIGHT / 2;
-	box_right = box_x + BOX_WIDTH / 2;
-	box_bottom = box_y + BOX_HEIGHT / 2;
+	box_left = box_x - width / 2;
+	box_top = box_y - height / 2;
+	box_right = box_x + width / 2;
+	box_bottom = box_y + height / 2;
 
 	circle_x = location.x;
 	circle_y = location.y;
@@ -285,4 +322,123 @@ bool ResultScene::HitBoxCircle(float box_x, float box_y, Vector2D location, floa
 		}
 	}
 	return hit_result;
+}
+
+void ResultScene::ChangePizzaAngle()
+{
+	pizza_angle += 0.01f;
+
+	if (pizza_angle >= 180.0f)
+	{
+		pizza_angle = 0.0f;
+	}
+}
+
+void ResultScene::HitCheck()
+{
+	if (cursor->GetPState() == P_State::attack && cursor->GetCanHit() == true)
+	{// ボタン or 星の上でクリックされたか？
+
+		// ボタン
+		if (HitBoxCircle(x1, y1, BOX_WIDTH, BOX_HEIGHT, cursor->GetLocation(), cursor->GetRadius()) == true)
+		{
+			select = 0;
+		}
+		else
+		{
+			if (HitBoxCircle(x2, y2, BOX_WIDTH, BOX_HEIGHT, cursor->GetLocation(), cursor->GetRadius()) == true)
+			{
+				select = 1;
+			}
+		}
+
+		// 星
+		if (HitBoxCircle(star_x[0], star_y[0], STAR_WIDTH, STAR_HEIGHT, cursor->GetLocation(), cursor->GetRadius()))
+		{
+			if (star_gold[0] == true && star_hp[0] != 0)
+			{
+				star_hp[0]--;
+			}
+			else
+			{
+				//鉄の音
+			}
+		}
+		else if (HitBoxCircle(star_x[1], star_y[1], STAR_WIDTH_2, STAR_HEIGHT_2, cursor->GetLocation(), cursor->GetRadius()))
+		{
+			if (star_gold[1] == true && star_hp[1] != 0)
+			{
+				star_hp[1]--;
+			}
+			else
+			{
+				//鉄の音
+			}
+		}
+		else if (HitBoxCircle(star_x[2], star_y[2], STAR_WIDTH, STAR_HEIGHT, cursor->GetLocation(), cursor->GetRadius()))
+		{
+			if (star_gold[2] == true && star_hp[2] != 0)
+			{
+				star_hp[2]--;
+			}
+			else
+			{
+				//鉄の音
+			}
+		}
+	}
+	else
+	{// カーソルがボタンの上にあるか？
+
+		if (HitBoxCircle(x1, y1, BOX_WIDTH, BOX_HEIGHT, cursor->GetLocation(), cursor->GetRadius()) == true)
+		{
+			on_button = 0;
+		}
+		else
+		{
+			if (HitBoxCircle(x2, y2, BOX_WIDTH, BOX_HEIGHT, cursor->GetLocation(), cursor->GetRadius()) == true)
+			{
+				on_button = 1;
+			}
+			else
+			{
+				on_button = -1;
+			}
+		}
+	}
+}
+
+void ResultScene::StarMove()
+{
+	if (star_hp[0] == 0)
+	{
+		if (star_x[0] <= -40.0f)
+		{
+			star_x[0] += 5.0f;
+			star_y[0] += 5.0f;
+
+			if (star_x[0] >= STAR_X)
+			{
+				star_hp[0] = 10;
+				star_x[0] = STAR_X;
+				star_y[0] = STAR_Y;
+			}
+		}
+		else
+		{
+			star_x[0] -= 5.0f;
+			star_y[0] -= 5.0f;
+		}
+	}
+
+	if (star_hp[1] == 0)
+	{
+		star_y[1] -= 5.0f;
+	}
+
+	if (star_hp[2] == 0)
+	{
+		star_x[2] += 5.0f;
+		star_y[2] -= 5.0f;
+	}
 }
--- KatiKatiClick/SceneFile/ResultScene.h
+++ KatiKatiClick/SceneFile/ResultScene.h
@@ -4,14 +4,17 @@
 #include "../ObjectFile/PlayerFile/Cursor.h"
 #include <vector>
 
-#define BOX_WIDTH  100
-#define BOX_HEIGHT 50
+#define BOX_WIDTH  100.0f
+#define BOX_HEIGHT 50.0f
 
-#define STAR_WIDTH 40.0f
-#define STAR_HEIGHT 40.0f
+#define STAR_WIDTH 60.0f
+#define STAR_HEIGHT 60.0f
 
-#define STAR_X 50.0f
-#define STAR_Y 250.0f
+#define STAR_WIDTH_2 100.0f
+#define STAR_HEIGHT_2 100.0f
+
+#define STAR_X 98.0f
+#define STAR_Y 235.0f
 
 #define PI    3.1415926535897932384626433832795f
 
@@ -24,11 +27,20 @@ private:
 	int on_button; // カーソルがボタンの上にあるか
 	bool is_clear; // クリアしたか？
 	int star_num;      // 星の数
+	int star_hp[3];
+	float star_x[3];
+	float star_y[3];
 	int bgm; // BGMデータ
 	bool is_bgm_active; // BGMが流れているか？
 	int star_images[2];
 	int fire_image;
-	double extrate; // 画像の拡大率
+	int pizza_image;
+	double pizza_angle;
+	double star_gold_extrate[3]; // 画像の拡大率
+	double star_angle[3];
+	double star_extrate[3];
+	bool star_gold[3];
+
 
 public:
 	ResultScene(bool is_game_clear,int goal_num);
@@ -40,5 +52,8 @@ public:
 
 private:
 	float DistanceSqrf(float box_x, float box_y, float circle_x, float circle_y);
-	bool HitBoxCircle(float box_x, float box_y, Vector2D location, float radius);//四角と円の当たり判定
+	bool HitBoxCircle(float box_x, float box_y, float width, float height, Vector2D location, float radius);//四角と円の当たり判定
+	void ChangePizzaAngle();
+	void HitCheck();
+	void StarMove();
 };
