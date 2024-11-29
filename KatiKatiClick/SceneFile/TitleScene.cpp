d3e584c1f45a149d07f03629f624fba219d8faf9
chihiro-hiro <120348424+chihiro-hiro@users.noreply.github.com>
2024-11-29 13:46:45 +0900

    タイトルボタンアニメーション修正

--- KatiKatiClick/SceneFile/TitleScene.cpp
+++ KatiKatiClick/SceneFile/TitleScene.cpp
@@ -8,17 +8,9 @@
 
 TitleScene::TitleScene()
 {
-	//x1 = 85.0f;
-	//x2 = 275.0f;
-	//y1 = 300.0f;
-	//y2 = 300.0f;
-
 	cursor = new Cursor;
 	opening_anim = new OpeningAnim;
-
 	select = -1;
-
-	//on_button = -1;
 
 	// ResourceManagerのインスタンスを取得
 	ResourceManager* rm = ResourceManager::GetInstance();
@@ -52,15 +44,11 @@ TitleScene::TitleScene()
 		end_button_img.push_back(tmp_img[i]);
 	}
 
-	//tmp_img = rm->GetImages("Resource/Images/Opening/End002.png");
-	//end_button_img.push_back(tmp_img[0]);
-
-
 	is_bgm_active = false;
 	
 	start_img_num = 0;
 	end_img_num = 0;
-	button_anim_count = 0;
+	change_wait_time = 0;
 
 	// ボタン座標の設定
 	start_x = SCREEN_WIDTH / 2;
@@ -88,25 +76,26 @@ void TitleScene::Update()
 		// BGM再生
 		PlaySoundMem(bgm, DX_PLAYTYPE_LOOP, TRUE);
 	}
+
 	if (opening_anim->GetAnimEnd() == false)
 	{
+		// オープニング更新処理
 		opening_anim->Update();
 		return;
 	}
+
+	// カーソルの更新処理
+	cursor->Update();
+
 	if (select != -1)
 	{
-		button_anim_count++;
-		if (button_anim_count < 4)
+		change_wait_time++;
+		if (change_wait_time < 60)
 		{
-			if (select == 0)
+			if (change_wait_time < 10)
 			{
-				// スタートボタンの画像切り替え
-				start_img_num = button_anim_count / 2 + 1;
-			}
-			else
-			{
-				// エンドボタンの画像切り替え
-				end_img_num = button_anim_count / 2 + 1;
+				// ボタン押下アニメション
+				ButtonAnimation();
 			}
 		}
 		else
@@ -114,47 +103,11 @@ void TitleScene::Update()
 			// 画面遷移して良い
 			change_screen_flg = true;
 		}
+
+		return;
 	}
 
-	cursor->Update();
-
-	//if (cursor->GetPState() == P_State::attack && cursor->GetCanHit() == true)
-	//{
-	//	if (HitBoxCircle(x1, y1, cursor->GetLocation(), cursor->GetRadius()) == true)
-	//	{
-	//		// startがクリックされた
-	//		select = 0;
-	//	}
-	//	else
-	//	{
-	//		if (HitBoxCircle(x2, y2, cursor->GetLocation(), cursor->GetRadius()) == true)
-	//		{
-	//			// endがクリックされた
-	//			select = 1;
-	//		}
-	//	}
-	//}
-	//else
-	//{
-	//	if (HitBoxCircle(x1, y1, cursor->GetLocation(), cursor->GetRadius()) == true)
-	//	{
-	//		// startの上にカーソルがある
-	//		on_button = 0;
-	//	}
-	//	else
-	//	{
-	//		if (HitBoxCircle(x2, y2, cursor->GetLocation(), cursor->GetRadius()) == true)
-	//		{
-	//			// endの上にカーソルがある
-	//			on_button = 1;
-	//		}
-	//		else
-	//		{
-	//			on_button = -1;
-	//		}
-	//	}
-	//}
-
+	// カーソルとボタンの当たり判定
 	if (cursor->GetPState() == P_State::attack && cursor->GetCanHit() == true)
 	{
 		if (HitBoxCircle(start_x, start_y, cursor->GetLocation(), cursor->GetRadius()) == true)
@@ -171,27 +124,6 @@ void TitleScene::Update()
 			}
 		}
 	}
-	//else
-	//{
-	//	if (HitBoxCircle(start_x, start_y, cursor->GetLocation(), cursor->GetRadius()) == true)
-	//	{
-	//		// startの上にカーソルがある
-	//		on_button = 0;
-	//	}
-	//	else
-	//	{
-	//		if (HitBoxCircle(end_x, end_y, cursor->GetLocation(), cursor->GetRadius()) == true)
-	//		{
-	//			// endの上にカーソルがある
-	//			on_button = 1;
-	//		}
-	//		else
-	//		{
-	//			on_button = -1;
-	//		}
-	//	}
-	//}
-
 }
 
 void TitleScene::Draw() const
@@ -237,66 +169,11 @@ void TitleScene::Draw() const
 	}
 	else
 	{
-		// 背景の雲
-		//DrawRotaGraph(60, GET_LANE_HEIGHT(4), 2, 0, cloud_img[0], TRUE);
-		//DrawRotaGraph(LANE_WIDTH * 3 - 60, GET_LANE_HEIGHT(2), 2, 0, cloud_img[0], TRUE);
-
 		// タイトルロゴ
 		DrawRotaGraph(SCREEN_WIDTH / 2, GET_LANE_HEIGHT(3), 1, 0, titlelogo_img[0], TRUE);
-
-		// スタートボタン画像の描画
-		DrawRotaGraphF(start_x, start_y, 1.0, 0.0, start_button_img[start_img_num], TRUE);
-		//DrawRotaGraphF(start_x, start_y, 1.0, 0.0, start_button_img[3], TRUE);
-		// エンドボタン画像の描画
-		DrawRotaGraphF(end_x, end_y, 1.0, 0.0, end_button_img[end_img_num], TRUE);
-		//DrawRotaGraphF(end_x, end_y, 1.0, 0.0, end_button_img[3], TRUE);
-
-
-		//switch (on_button)
-		//{
-		//case 0:
-		//	// スタートにカーソルが重なっているとき
-		//	DrawBoxAA(x1 - (float)BOX_WIDTH / 2, y1 - (float)BOX_HEIGHT / 2, x1 + (float)BOX_WIDTH / 2, y1 + (float)BOX_HEIGHT / 2, 0x999999, TRUE);
-		//	DrawString((x1 - (float)BOX_WIDTH / 2) + 30, (y1 - (float)BOX_HEIGHT / 2) + 20, "START", 0x000000);
-		//	DrawBoxAA(x2 - (float)BOX_WIDTH / 2, y2 - (float)BOX_HEIGHT / 2, x2 + (float)BOX_WIDTH / 2, y2 + (float)BOX_HEIGHT / 2, 0xffffff, TRUE);
-		//	DrawString((x2 - (float)BOX_WIDTH / 2) + 35, (y2 - (float)BOX_HEIGHT / 2) + 20, "END", 0x000000);
-
-		//	// スタートボタンにカーソルが重なっているとき
-		//	// 描画輝度のセット
-		//	SetDrawBright(128, 128, 128);
-		//	//  スタートボタンボタン画像の描画
-		//	//DrawRotaGraphF(start_x, start_y, 1.0, 0.0, start_button_img[start_img_num], TRUE);
-		//	// 描画輝度を元に戻す
-		//	SetDrawBright(255, 255, 255);
-
-		//	break;
-
-		//case 1:
-		//	// エンドにカーソルが重なっているとき
-		//	DrawBoxAA(x1 - (float)BOX_WIDTH / 2, y1 - (float)BOX_HEIGHT / 2, x1 + (float)BOX_WIDTH / 2, y1 + (float)BOX_HEIGHT / 2, 0xffffff, TRUE);
-		//	DrawString((x1 - (float)BOX_WIDTH / 2) + 30, (y1 - (float)BOX_HEIGHT / 2) + 20, "START", 0x000000);
-		//	DrawBoxAA(x2 - (float)BOX_WIDTH / 2, y2 - (float)BOX_HEIGHT / 2, x2 + (float)BOX_WIDTH / 2, y2 + (float)BOX_HEIGHT / 2, 0x999999, TRUE);
-		//	DrawString((x2 - (float)BOX_WIDTH / 2) + 35, (y2 - (float)BOX_HEIGHT / 2) + 20, "END", 0x000000);
-
-		//	// エンドボタンにカーソルが重なっているとき
-		//	// 描画輝度のセット
-		//	SetDrawBright(128, 128, 128);
-		//	// エンドボタンボタン画像の描画
-		//	//DrawRotaGraphF(end_x, end_y, 1.0, 0.0, end_button_img[end_img_num], TRUE);
-		//	// 描画輝度を元に戻す
-		//	SetDrawBright(255, 255, 255);
-
-		//	break;
-
-		//default:
-		//	// ボタンの上にカーソルが重なっていないとき
-		//	DrawBoxAA(x1 - (float)BOX_WIDTH / 2, y1 - (float)BOX_HEIGHT / 2, x1 + (float)BOX_WIDTH / 2, y1 + (float)BOX_HEIGHT / 2, 0xffffff, TRUE);
-		//	DrawString((x1 - (float)BOX_WIDTH / 2) + 30, (y1 - (float)BOX_HEIGHT / 2) + 20, "START", 0x000000);
-		//	DrawBoxAA(x2 - (float)BOX_WIDTH / 2, y2 - (float)BOX_HEIGHT / 2, x2 + (float)BOX_WIDTH / 2, y2 + (float)BOX_HEIGHT / 2, 0xffffff, TRUE);
-		//	DrawString((x2 - (float)BOX_WIDTH / 2) + 35, (y2 - (float)BOX_HEIGHT / 2) + 20, "END", 0x000000);
-		//	break;
-		//}
-
+		
+		// ボタンの描画
+		DrawButton();
 
 		cursor->Draw();
 	}
@@ -418,4 +295,79 @@ bool TitleScene::HitBoxCircle(float box_x, float box_y, Vector2D location, float
 		}
 	}
 	return hit_result;
+}
+
+// ボタン押下アニメション処理
+void TitleScene::ButtonAnimation()
+{
+	if (select == 0)
+	{
+		if (change_wait_time < 8)
+		{
+			// スタートボタンの画像切り替え
+			start_img_num = change_wait_time / 4 + 1;
+		}
+		else
+		{
+			start_img_num = 1;
+		}
+	}
+	else
+	{
+		if (change_wait_time < 8)
+		{
+			// エンドボタンの画像切り替え
+			end_img_num = change_wait_time / 4 + 1;
+		}
+		else
+		{
+			end_img_num = 1;
+		}
+	}
+}
+
+// ボタンの描画
+void TitleScene::DrawButton() const
+{
+	if (change_wait_time > 8)
+	{
+		switch (select)
+		{
+		case 0:
+			// スタートボタンを暗くする
+			// 描画輝度のセット
+			SetDrawBright(128, 128, 128);
+			// スタートボタン画像の描画
+			DrawRotaGraphF(start_x, start_y, 1.0, 0.0, start_button_img[start_img_num], TRUE);
+			// 描画輝度を元に戻す
+			SetDrawBright(255, 255, 255);
+
+			// エンドボタン画像の描画
+			DrawRotaGraphF(end_x, end_y, 1.0, 0.0, end_button_img[end_img_num], TRUE);
+			break;
+
+		case 1:
+			// スタートボタン画像の描画
+			DrawRotaGraphF(start_x, start_y, 1.0, 0.0, start_button_img[start_img_num], TRUE);
+
+			// エンドボタンを暗くする
+			// 描画輝度のセット
+			SetDrawBright(128, 128, 128);
+			// エンドボタン画像の描画
+			DrawRotaGraphF(end_x, end_y, 1.0, 0.0, end_button_img[end_img_num], TRUE);
+			// 描画輝度を元に戻す
+			SetDrawBright(255, 255, 255);
+			break;
+
+		default:
+			break;
+		}
+	}
+	else
+	{
+		// スタートボタン画像の描画
+		DrawRotaGraphF(start_x, start_y, 1.0, 0.0, start_button_img[start_img_num], TRUE);
+		// エンドボタン画像の描画
+		DrawRotaGraphF(end_x, end_y, 1.0, 0.0, end_button_img[end_img_num], TRUE);
+	}
 }
