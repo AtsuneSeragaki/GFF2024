41a9081ed7822da7d202df999c0448b4b388d347
chihiro-hiro <120348424+chihiro-hiro@users.noreply.github.com>
2024-12-03 17:17:46 +0900

    コインUIの位置修正

--- KatiKatiClick/UtilityFile/Define.h
+++ KatiKatiClick/UtilityFile/Define.h
@@ -7,8 +7,10 @@
 #define Sqiosh_Enemy_MAX 20
 
 #define SCREEN_WIDTH 360
-
 #define SCREEN_HEIGHT 800
+
+#define SCREEN_WIDTH_HALF (SCREEN_WIDTH / 2)
+#define SCREEN_HEIGHT_HALF (SCREEN_HEIGHT / 2)
 
 #define ONE_LANE_HEIGHT (SCREEN_HEIGHT / 10)//横切りのレーンの高さ
 
